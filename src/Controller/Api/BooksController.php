afa3d57bb4eeaf2d78478cee41d73f38c5118f78
Jeroen <jvandijkgarcia@gmail.com>
2021-08-01 14:15:28 +0200

    Añadida funcionalidad post a la api.

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,5 +19,18 @@ class BooksController extends AbstractFOSRestController
     public function getActions(BookRepository $br)
     {
         return $br->findAll();
+    }
+    /**
+     * @Rest\Post(path="/books")
+     * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
+     * 
+     */
+    public function postAction(EntityManagerInterface $em, Request $request)
+    {
+        $book = new Book();
+        $book->setTitle($request->get('title'));
+        $em->persist($book);
+        $em->flush();
+        return $book;
     }
 }
