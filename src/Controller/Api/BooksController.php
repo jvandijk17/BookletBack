cfb5a641d3884449511a3e8091542d2ff22093ba
Jeroen <jvandijkgarcia@gmail.com>
2021-08-02 13:32:55 +0200

    Extraída Lógica subir imagenes a servicio reutilizable.

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -6,12 +6,12 @@ use App\Entity\Book;
 use App\Form\Model\BookDto;
 use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
-use League\Flysystem\FilesystemOperator;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -29,16 +29,13 @@ class BooksController extends AbstractFOSRestController
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      * 
      */
-    public function postAction(EntityManagerInterface $em, Request $request, FilesystemOperator $defaultStorage)
+    public function postAction(EntityManagerInterface $em, Request $request, FileUploader $defaultStorage)
     {
         $bookDto = new BookDto();
         $form = $this->createForm(BookFormType::class, $bookDto);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $extension = explode('/', mime_content_type($bookDto->base64Image))[1];
-            $data = explode(',', $bookDto->base64Image);
-            $path = sprintf('/books/%s.%s', uniqid('book_', true), $extension);        
-            $defaultStorage->write($path, base64_decode($data[1]));
+        if ($form->isSubmitted() && $form->isValid()) {            
+            $path = $defaultStorage->uploadBase64File($bookDto->base64Image);
             $book = new Book();
             $book->setTitle($bookDto->title);
             $book->setImage($path);
---
+++ src/Service/FileUploader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use League\Flysystem\FilesystemOperator;
+
+class FileUploader
+{
+
+    private $defaultStorage;
+
+    public function __construct(FilesystemOperator $defaultStorage)
+    {
+        $this->defaultStorage = $defaultStorage;
+    }
+
+    public function uploadBase64File(string $base64File): string
+    {
+        $extension = explode('/', mime_content_type($base64File))[1];
+        $data = explode(',', $base64File);
+        $path = sprintf('/books/%s.%s', uniqid('book_', true), $extension);
+        $this->defaultStorage->write($path, base64_decode($data[1]));
+        return $path;
+    }
+}
